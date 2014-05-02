6a67b873461e5ef8a06909381f6eb4f5991771c2
Alberto Ruiz <aruiz@gnome.org>
2014-05-02 19:46:19 +0100

    Fix back button and URL handling

--- js/namespace.js
+++ js/namespace.js
@@ -1,4 +1,5 @@
 var ns = "";
+var klass = "";
 
 function createLink (url, content) {
   return "<a href=\"" +url+ "\">" +content+ "</a>";
@@ -30,19 +31,29 @@ function clearItems (lists)
 
 function goToClass (className) {
   $.getJSON ("docs-test/" + ns + "." + className + ".json", function (data) {
-    clearItems (["#class-properties-list", "#class-methods-list", "#class-signals-list", "#class-constructors-list"]);
+    clearItems (["#class-properties-list",
+                 "#class-methods-list",
+                 "#class-signals-list",
+                 "#class-constructors-list"]);
     hideEverything ();
+    $("#class-area ul #namespace").html(ns);
+    $("#class-area ul #class-name").html(data.name);
+    $("#class-doc").html(data.doc);
+
+    //$.each ($("span[data-xref]")
     showArea ("#class-area");
+
+    method_prefix = ns + "." + data.name + ".prototype.";
 
     var tmpString = "";
     $.each (data.constructors, function (index, item) {
-      tmpString = tmpString + "<li>" + item.name + "()</li>";
+      tmpString = tmpString + "<li>" + method_prefix + item.name + "()</li>";
     });
     $("#class-constructors-list").html(tmpString);
 
     tmpString = "";
     $.each (data.methods, function (index, item) {
-      tmpString = tmpString + "<li>" + item.name + "()</li>";
+      tmpString = tmpString + "<li>" + method_prefix + item.name + "()</li>";
     });
     $("#class-methods-list").html(tmpString);
 
@@ -60,14 +71,23 @@ function goToClass (className) {
   });
 }
 
-function onHashchange () {
+function goToNS ()
+{
+  hideEverything();
+  showArea ("#namespace-area");
+}
+
+function onHashchange (newUrl) {
   var hash = location.hash;
   if (hash.match("^#class-"))
     goToClass(hash.replace ("#class-", ""));
+  else
+    goToNS ();
 }
 
 $(document).ready (function () {
   window.onhashchange = onHashchange;
+  goToNS ();
 
   $("#class-list").html("");
   var content = "";
